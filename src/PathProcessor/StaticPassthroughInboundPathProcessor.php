0f955fecba8f5dcbbd9839730c1d2f841cb973c2
Simon Mellerin <simon.mellerin@makina-corpus.com>
2023-07-26 11:51:48 +0200

    Add StaticPassthroughOutboundPathProcessor

---
+++ src/PathProcessor/StaticPassthroughInboundPathProcessor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\static_passthrough\PathProcessor;
+
+use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class StaticPassthroughInboundPathProcessor implements InboundPathProcessorInterface {
+
+  public function processInbound($path, Request $request) {
+    $config = \Drupal::config('static_passthrough.settings');
+
+    foreach ($config->get('static_passthrough.directories') as $directory) {
+      $base_path = '/' . $directory['root_path'] . '/';
+
+      if (\strpos($path, $base_path) === 0) {
+        $escaped_root_path = \str_replace('/','\/', $base_path);
+        $relative_path = \preg_replace('|^' . $escaped_root_path . '|', '', $path);
+        $relative_path = \str_replace('/',':', $relative_path);
+        return \sprintf("%s%s", $base_path, $relative_path);
+      }
+    }
+
+    return $path;
+  }
+}
--- src/PathProcessor/StaticPassthroughOutboundPathProcessor.php
+++ src/PathProcessor/StaticPassthroughOutboundPathProcessor.php
@@ -2,12 +2,13 @@
 
 namespace Drupal\static_passthrough\PathProcessor;
 
-use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
+use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
+use Drupal\Core\Render\BubbleableMetadata;
 use Symfony\Component\HttpFoundation\Request;
 
-class StaticPassthroughPathProcessor implements InboundPathProcessorInterface {
+class StaticPassthroughOutboundPathProcessor implements OutboundPathProcessorInterface {
 
-  public function processInbound($path, Request $request) {
+  public function processOutbound($path, &$options = [], Request $request = NULL, BubbleableMetadata $bubbleable_metadata = NULL) {
     $config = \Drupal::config('static_passthrough.settings');
 
     foreach ($config->get('static_passthrough.directories') as $directory) {
@@ -16,7 +17,7 @@ class StaticPassthroughPathProcessor implements InboundPathProcessorInterface {
       if (\strpos($path, $base_path) === 0) {
         $escaped_root_path = \str_replace('/','\/', $base_path);
         $relative_path = \preg_replace('|^' . $escaped_root_path . '|', '', $path);
-        $relative_path = \str_replace('/',':', $relative_path);
+        $relative_path = \str_replace(':','/', $relative_path);
         return \sprintf("%s%s", $base_path, $relative_path);
       }
     }
